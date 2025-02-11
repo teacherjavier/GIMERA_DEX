7e97c8914203ab923e2aaa456dc2a899a1e5d9e5
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2025-02-11 19:08:13 +0100

    ai address can execute functions on behalf of users, and when the aiAddress is passed, the user's wallet address is used to perform the function

--- contracts/contracts/POOL1.sol
+++ contracts/contracts/POOL1.sol
@@ -4,30 +4,40 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract Pool1 is ERC20 {
     using SafeERC20 for IERC20;
 
     IERC20 public tokenA;
     IERC20 public tokenB;
     address public aiAddress;
+    address public owner;
     uint256 public constant FEE_RATE = 30; // 0.3%
+
+    modifier onlyAuthorized(address user) {
+        require(msg.sender == owner || msg.sender == aiAddress || msg.sender == user, "Not authorized");
+        _;
+    }
 
     constructor(address _tokenA, address _tokenB, address _aiAddress) ERC20("Liquidity Provider Token 1", "LP_1") {
         tokenA = IERC20(_tokenA);
         tokenB = IERC20(_tokenB);
         aiAddress = _aiAddress;
+        owner = msg.sender;
 
         // Initial reserves
         tokenA.safeTransferFrom(msg.sender, address(this), 2000 * 10**decimals());
         tokenB.safeTransferFrom(msg.sender, address(this), 1000 * 10**decimals());
     }
 
-    function swap(uint256 amountIn, address fromToken, address toToken) external {
+    function swap(uint256 amountIn, address fromToken, address toToken, address user) external onlyAuthorized(user) {
         require(fromToken == address(tokenA) || fromToken == address(tokenB), "Invalid fromToken");
         require(toToken == address(tokenA) || toToken == address(tokenB), "Invalid toToken");
 
+        address sender = msg.sender == aiAddress ? user : msg.sender;
+
         // Transfer the amount from sender to pool
-        IERC20(fromToken).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(fromToken).safeTransferFrom(sender, address(this), amountIn);
 
         uint256 amountOut = getAmountOut(amountIn, fromToken, toToken);
 
@@ -36,7 +46,7 @@ contract Pool1 is ERC20 {
         uint256 amountOutAfterFee = amountOut - fee;
 
         // Transfer the amount out to sender
-        IERC20(toToken).safeTransfer(msg.sender, amountOutAfterFee);
+        IERC20(toToken).safeTransfer(sender, amountOutAfterFee);
     }
 
     function getAmountOut(uint256 amountIn, address fromToken, address toToken) public view returns (uint256) {
@@ -46,17 +56,19 @@ contract Pool1 is ERC20 {
         return (amountInWithFee * reserveTo) / (reserveFrom + amountInWithFee);
     }
 
-    function addLiquidity(uint256 amountA, uint256 amountB) external {
-        tokenA.safeTransferFrom(msg.sender, address(this), amountA);
-        tokenB.safeTransferFrom(msg.sender, address(this), amountB);
-        _mint(msg.sender, amountA + amountB);
+    function addLiquidity(uint256 amountA, uint256 amountB, address user) external onlyAuthorized(user) {
+        address sender = msg.sender == aiAddress ? user : msg.sender;
+        tokenA.safeTransferFrom(sender, address(this), amountA);
+        tokenB.safeTransferFrom(sender, address(this), amountB);
+        _mint(sender, amountA + amountB);
     }
 
-    function removeLiquidity(uint256 liquidity) external {
-        _burn(msg.sender, liquidity);
+    function removeLiquidity(uint256 liquidity, address user) external onlyAuthorized(user) {
+        address sender = msg.sender == aiAddress ? user : msg.sender;
+        _burn(sender, liquidity);
         uint256 amountA = (liquidity * tokenA.balanceOf(address(this))) / totalSupply();
         uint256 amountB = (liquidity * tokenB.balanceOf(address(this))) / totalSupply();
-        tokenA.safeTransfer(msg.sender, amountA);
-        tokenB.safeTransfer(msg.sender, amountB);
+        tokenA.safeTransfer(sender, amountA);
+        tokenB.safeTransfer(sender, amountB);
     }
 }
